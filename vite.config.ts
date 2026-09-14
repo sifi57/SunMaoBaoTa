7f0884881985681b2a9f415e8d4a677d3c2c92eb
sifi57 <107749025+sifi57@users.noreply.github.com>
2026-09-15 02:09:12 +0800

    chore: update asset paths and vite configuration
    Switch to relative paths for external resources to ensure correct loading in non-root environments and remove redundant React plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     base: './',
-    plugins: [react(), tailwindcss()],
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
